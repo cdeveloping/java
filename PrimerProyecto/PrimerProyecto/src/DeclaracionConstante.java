76d5ff29b1f96b34ce6999c5ca84d9a780813087
Carlos <carlosdeveloper@outlook.es>
2024-07-21 21:36:55 +0200

    Constante
    Declaración de una constante

---
+++ PrimerProyecto/PrimerProyecto/src/DeclaracionConstante.java
@@ -0,0 +1,18 @@
+public class DeclaracionConstante {
+    public static void main(String[] args) {
+        final int a = 8;
+
+        System.out.println(a);
+
+
+        //El siguiente código dará un error y no nos va a permitir ejecutar el código.
+
+        // a = 14;
+
+        
+
+
+
+
+    }
+}
