1662641c6f866ef6e241d15179a20b0f0dd4a148
Carlos <carlosdeveloper@outlook.es>
2024-08-05 19:37:08 +0200

    Clase Math
    Primeros ejemplos de la clase Math

---
+++ PrimerProyecto/PrimerProyecto/src/calculos_conMath.java
@@ -0,0 +1,20 @@
+public class calculos_conMath {
+
+
+    public static void main(String[] args) {
+        
+        //Calcular la raíz cuadrada de 9
+        double raiz_cuadrada = Math.sqrt(9);
+
+        System.out.println(raiz_cuadrada);
+
+        //Método para redondear
+        double redondeo = 5.85;
+
+        float redondeado = Math.round(redondeo);
+
+        System.out.println(redondeado);
+
+
+    }
+}
